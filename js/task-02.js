9748283ede6f927e2488d002d2e9e1709f349a28
Shulepa Volodymyr <shulepatrader@gmail.com>
2022-10-31 17:26:50 +0200

    fix

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const listItems = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+
+  li.textContent = ingredient;
+  li.classList.add("item");
+
+  return li;
+});
+
+list.append(...listItems);
+
+console.log(list);
