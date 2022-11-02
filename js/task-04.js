b52eedd891b6f087db1f110f498c01a5a96f3d10
Shulepa Volodymyr <shulepatrader@gmail.com>
2022-11-02 12:10:28 +0200

    task 5 complete

--- js/task-04.js
+++ js/task-04.js
@@ -1,9 +1,6 @@
 let counterValue = 0;
-
 const decrementBtn = document.querySelector('[data-action="decrement"]');
-
 const incrementBtn = document.querySelector('[data-action="increment"]');
-
 const valueEl = document.querySelector("#value");
 
 decrementBtn.addEventListener("click", function () {
